941721fa482fdfca5bbfa0f2d58714bc8ebfc9d4
Barbara <kaluzabarbara26@gmail.com>
2025-01-27 16:28:40 +0100

    change image on home page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ function Header() {
         <div className="header-wrapper">
             <h1>Witaj!</h1>
             <h2 className="header">W Twojej książce kucharskiej 🧑‍🍳</h2>
-            <img className="img" src="https://cdn.pixabay.com/photo/2020/02/22/08/41/mushrooms-4869880_960_720.jpg" alt="cooking"/>
+            <img className="img" src="./imgage-home.png" alt="food"></img>
 
         </div>
          
