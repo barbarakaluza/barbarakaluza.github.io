1c7a34c9517520ab9687ddf4bede8c41f6cb5f00
Barbara <kaluzabarbara26@gmail.com>
2025-01-27 16:51:06 +0100

    chagne styles on home page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
+import image from './image-home.png';
+
 function Header() {
     return (
         <div className="header-wrapper">
             <h1>Witaj!</h1>
             <h2 className="header">W Twojej książce kucharskiej 🧑‍🍳</h2>
-            <img className="img" src="./imgage-home.png" alt="food"></img>
+            <img className="img" src={image} alt="food" />
 
         </div>
          
