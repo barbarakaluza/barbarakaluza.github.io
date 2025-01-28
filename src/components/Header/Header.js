10116a96ec030b7f9c50a1ba130ea3373633fddb
Barbara <kaluzabarbara26@gmail.com>
2025-01-29 00:06:53 +0100

    adding new responsive styles to home page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,16 @@
-import image from './image-home.png';
+import image1 from './image-home.png';  
+import image2 from './image-home-2.png';
 
 function Header() {
     return (
         <div className="header-wrapper">
             <h1>Witaj!</h1>
             <h2 className="header">W Twojej książce kucharskiej 🧑‍🍳</h2>
-            <img className="img" src={image} alt="food" />
+            
+            <div className="image-container">
+                <img className="img" src={image1} alt="food 1" />
+                <img className="img" src={image2} alt="food 2" />
+            </div>
 
         </div>
          
